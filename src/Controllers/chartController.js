e60157cfb62d32634ea9d6bf1f21d35334461904
Caleb Gammon <calebwgammon@gmail.com>
2021-01-06 18:32:54 -0500

    tested getChartData endpoint and res is sending data from AV

--- src/Controllers/chartController.js
+++ src/Controllers/chartController.js
@@ -1,11 +1,22 @@
 const axios = require('axios')
-const {AV_API_KEY} = require('../config')
+const {AV_API_KEY, AV_API_URL_BASE} = require('../config')
 
 exports.getChartData = (req, res, next) => {
-    res.send('Chart Data: Success!!!')
+    let stockSymbol = req.query.symbol
+    console.log('params: ', stockSymbol)
+    axios.get(AV_API_URL_BASE + 'function=TIME_SERIES_DAILY_ADJUSTED' + '&symbol=' + stockSymbol + '&apikey=' + AV_API_KEY )
+        .then(response => {
+            res.send(response.data)
+        })
+        .catch(err => {
+            console.log('THE ERROR: ', err)
+            next();
+        })
+    
 }
 
 exports.GetSMAData = (req, res, next) => {
+
     res.send('SMA Data: Success!!!')
 }
 
