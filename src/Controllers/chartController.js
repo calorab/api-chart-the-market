a968b1dd8368fa3ad4f6a5addd5703cd18beab6e
Caleb Gammon <calebwgammon@gmail.com>
2021-01-11 19:05:57 -0500

    added SMA/EMA/MACD endpoint functionality and started data limiting middleware

--- src/Controllers/chartController.js
+++ src/Controllers/chartController.js
@@ -16,21 +16,55 @@ exports.getChartData = (req, res, next) => {
 }
 
 exports.GetSMAData = (req, res, next) => {
-    // let stockSymbol = req.query.symbol
-    // let interval = req.query.interval
-    // console.log('params: ', stockSymbol)
-    // axios.get(AV_API_URL_BASE + 'function=SMA' + '&symbol=' + stockSymbol + '&interval=' + interval + '&apikey=' + AV_API_KEY )
-    //     .then(response => {
-    //         console.log('Response in CD: ', response.data)
-    //         res.send(response.data)
-    //     })
-    //     .catch(err => {
-    //         console.log('THE ERROR: ', err)
-    //         next();
-    //     })
+    let stockSymbol = req.query.symbol
+    let interval = req.query.interval
+    let timePeriod = req.query.time_period
+    console.log('params: ', stockSymbol, interval, timePeriod)
+    axios.get(AV_API_URL_BASE + 'function=SMA' + '&symbol=' + stockSymbol + '&interval=' + interval + '&time_period=' + timePeriod + '&series_type=close' + '&apikey=' + AV_API_KEY )
+        .then(response => {
+            console.log('Response in CD: ', response.data)
+            res.send(response.data)
+        })
+        .catch(err => {
+            console.log('THE ERROR: ', err)
+            next();
+        })
     // res.send('SMA Data: Success!!!')
 }
 
 exports.GetEMAData = (req, res, next) => {
-    res.send('EMA Data: Success!!!')
+    let stockSymbol = req.query.symbol
+    let interval = req.query.interval
+    let timePeriod = req.query.time_period
+    console.log('params: ', stockSymbol, interval, timePeriod)
+    axios.get(AV_API_URL_BASE + 'function=EMA' + '&symbol=' + stockSymbol + '&interval=' + interval + '&time_period=' + timePeriod + '&series_type=close' + '&apikey=' + AV_API_KEY )
+        .then(response => {
+            console.log('Response in CD: ', response.data)
+            res.send(response.data)
+        })
+        .catch(err => {
+            console.log('THE ERROR: ', err)
+            next();
+        })
+    // res.send('EMA Data: Success!!!')
 } 
+
+exports.GetMACDData = (req, res, next) => {
+    let stockSymbol = req.query.symbol
+    let interval = req.query.interval
+    console.log('params: ', stockSymbol, interval)
+    axios.get(AV_API_URL_BASE + 'function=MACD' + '&symbol=' + stockSymbol + '&interval=' + interval + '&series_type=close' + '&apikey=' + AV_API_KEY )
+        .then(response => {
+            let parsedData = response.data["Technical Analysis: MACD"]["2021-01-11"]
+            console.log(parsedData)
+            // let skinnyData
+            // for (let i = 0; i < response.datalength && i < 100; i++) {
+
+            // }
+            res.send(parsedData)
+        })
+        .catch(err => {
+            console.log('THE ERROR: ', err)
+            next();
+        })
+}
--- src/Routes/chartRoute.js
+++ src/Routes/chartRoute.js
@@ -12,5 +12,8 @@ router.get('/sma', chartController.GetSMAData)
 
 // GET /ema (Exponential Moving Average endpoint)
 router.get('/ema', chartController.GetEMAData)
+
+// GET /macd (Moving Average Convergence/Divergence endpoint)
+router.get('/macd', chartController.GetMACDData)
  
 module.exports = router
